b1ed0fb2d8725294956c201ba56d91203226319b
Lucas Pires <lucaspiressimao@gmail.com>
2015-03-17 11:27:15 -0300

    Fixed identation

--- rsa_ex.c
+++ rsa_ex.c
@@ -16,43 +16,56 @@ void main()
 { 
     printf("\nENTER FIRST PRIME NUMBER\n"); 
     scanf("%d",&p); 
-    flag=prime(p); 
-    if(flag==0) 
+
+    // Check if it is prime
+    if( prime(p) == 0 ) 
     { 
         printf("\nWRONG INPUT\n"); 
         exit(1); 
     } 
+
     printf("\nENTER ANOTHER PRIME NUMBER\n"); 
     scanf("%d",&q); 
-    flag=prime(q); 
-    if(flag==0||p==q) 
+
+    // Check if it is prime
+    if( prime(q) == 0 || p == q ) 
     { 
         printf("\nWRONG INPUT\n"); 
         exit(1); 
     } 
+
     printf("\nENTER MESSAGE\n"); 
     fflush(stdin); 
     scanf("%s",msg); 
-    for(i=0;msg[i]!=NULL;i++) 
-    m[i]=msg[i]; 
+
+    // Copy message
+    for( i=0 ; msg[i]!=NULL ; i++ ) 
+        m[i]=msg[i]; 
+    
     n=p*q; 
     t=(p-1)*(q-1); 
     ce(); 
+
     printf("\nPOSSIBLE VALUES OF e AND d ARE\n"); 
-    for(i=0;i<j-1;i++) 
-    printf("\n%ld\t%ld",e[i],d[i]); 
+    
+    for( i=0 ; i<j-1 ; i++ ) 
+        printf("\n%ld\t%ld",e[i],d[i]); 
+    
     encrypt(); 
     decrypt(); 
 }
 
+// Check if they are prime
 int prime(long int pr) 
 { 
     int i; 
+
     j = sqrt(pr); 
+    
     for(i=2;i<=j;i++) 
     { 
         if(pr%i==0) 
-        return 0; 
+            return 0; 
     } 
     return 1; 
 } 
@@ -60,83 +73,101 @@ int prime(long int pr)
 void ce() 
 { 
     int k; 
+
     k=0; 
-    for(i=2;i<t;i++) 
+
+    for( i=2 ; i<t ; i++ ) 
     { 
         if(t%i==0) 
-        continue; 
-        flag=prime(i); 
-        if(flag==1&&i!=p&&i!=q) 
+            continue; 
+
+        if( (prime(i) == 1) && (i != p) && (i != q)) 
         { 
             e[k]=i; 
-            flag=cd(e[k]); 
-            if(flag>0) 
+
+            if( cd(e[k]) > 0 ) 
             { 
-                d[k]=flag; 
+                d[k] = cd(e[k]); 
                 k++; 
             } 
-            if(k==99) 
-            break; 
+
+            if(k == 99) 
+                break;
         } 
     } 
 } 
 
 long int cd(long int x) 
 { 
-    long int k=1; 
+    long int k = 1; 
+
     while(1) 
     { 
-        k=k+t; 
-        if(k%x==0) 
-        return(k/x); 
+        k = k+t; 
+        if( (k % x) == 0 ) 
+            return( k / x ); 
     } 
 } 
 
 void encrypt() 
 { 
     long int pt,ct,key=e[0],k,len; 
-    i=0; 
-    len=strlen(msg); 
-    while(i!=len) 
+
+    i = 0; 
+    len = strlen(msg); 
+    
+    while( i != len ) 
     { 
-        pt=m[i]; 
-        pt=pt-96; 
-        k=1; 
-        for(j=0;j<key;j++) 
+        pt = m[i]; 
+        pt = pt-96; 
+        k = 1; 
+
+        for( j=0 ; j<key ; j++ ) 
         { 
-            k=k*pt; 
-            k=k%n; 
+            k = k * pt;
+            k = k % n; 
         } 
-        temp[i]=k; 
-        ct=k+96; 
-        en[i]=ct; 
+
+        temp[i] = k; 
+        ct = k + 96; 
+        en[i] = ct; 
         i++; 
     } 
-    en[i]=-1; 
+
+    en[i] = -1; 
+
     printf("\nTHE ENCRYPTED MESSAGE IS\n"); 
+    
     for(i=0;en[i]!=-1;i++) 
-    printf("%c",en[i]); 
+        printf("%c",en[i]); 
 } 
 
 void decrypt() 
 { 
-    long int pt,ct,key=d[0],k; 
+    long int pt,ct,key = d[0],k; 
+  
     i=0; 
-    while(en[i]!=-1) 
+  
+    while(en[i] != -1) 
     { 
-        ct=temp[i]; 
-        k=1; 
-        for(j=0;j<key;j++) 
+        ct = temp[i]; 
+        k = 1; 
+
+        for( j=0 ; j<key ; j++ )
         { 
-            k=k*ct; 
-            k=k%n; 
-        } 
-        pt=k+96; 
-        m[i]=pt; 
+            k = k * ct;
+            k = k % n; 
+        }
+
+        pt = k + 96; 
+        m[i] = pt; 
         i++; 
-    } 
-    m[i]=-1; 
+    }
+
+    m[i] = -1; 
+
     printf("\nTHE DECRYPTED MESSAGE IS\n"); 
+    
     for(i=0;m[i]!=-1;i++) 
        printf("%c",m[i]); 
 }
